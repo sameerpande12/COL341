61ddc4f27ecb79460c46b4b6e35bbc1a4d54e588
Sameer <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-10-05 13:17:59 +0530

    some functions for dt"

--- A4/dt.py
+++ A4/dt.py
@@ -11,27 +11,82 @@ import numpy as np
 import csv 
 
 
+continuous= ["Age","Fnlwgt","Education Number","Capital Gain","Capital Loss","Hour per Week"]
 
-class one_hot_encoder:
-    def __init__(self,labels):
-        self.labels=labels#two dimenstional array
-    
-    def encode(self,x_input):
-        x_output = []
-        col_index = 0
-        for i in range((self.labels).size):
-            if( (self.labels[i]).size == 0):
-                x_output.append(x_input[:,col_index])
-                
-            else:
-                for label in self.labels[i]:
-                    x_output.append(x_input[:,col_index]==label)
+
+
+class Node:
+        def __init__(self,depth):
+            self.index
+            self.labels
+            self.children = []
+            self.depth = depth
             
-            col_index= col_index+1
-                
+        def setLabels(self,labels,label_counts):
+            self.labels = labels
+            self.label_counts = label_counts
+            
+        def setSplitter(self,index,numBins=5):
+            self.index = index
+            
+            if self.labels[index] in continuous:
+                numChildren = numBins
+            else:
+                numChildren = self.label_counts
+            for i in range(numChildren):
+                self.children.append(Node(self.depth+1))
         
-        return np.array(x_output)
+    
+    
 
+def getEntropy(y):
+    unique,counts = np.unique(y,return_counts=True)
+    h = 0
+    for i in range(unique.size):
+        p = counts[i]/np.sum(counts)
+        if p > 0:
+            h = h - p*np.log(p)
+
+def getInfoGain(index,labels,x,numBins=5):##x has no header. x's last column is result
+    IG  = 0
+    IG  = getEntropy(x)
+    if labels[index] in continuous:        
+        minimum = min(x[:,index])
+        maximum = min(x[:,index])
+        delta = (maximum - minimum)/numBins
+        for i in range(numBins):
+            lower_lim = minimum + i * delta
+            upper_lim = lower_lim + delta
+            if i ==0:
+                child = x[x[:,index]<=upper_lim]
+            else:
+                child = x[x[:,index]>lower_lim]
+                child = child[child[:,index]<=upper_lim]
+            h_child = getEntropy(child[:,-1])
+            p_child = child.shape[0]/x.shape[0]
+            IG = IG - p_child * h_child
+        
+    else:
+        attributes  = np.unique(x[:,index])        
+        for attribute in attributes:
+            child = x[x[:,index]==attribute]
+            h_child = getEntropy(child[:,-1])
+            p_child = child.shape[0]/x.shape[0]
+            IG = IG  - p_child * h_child
+        
+    return IG        
+
+def getSplitIndex(labels,x):### x has no header. x's last column stands for output label. labels have headers for all the columns excluding output
+    maxIG = getInfoGain(0,labels,x)
+    maxIndex = 0
+    for index in range(len(labels)):
+        IG = getInfoGain(index,labels,x)
+        if IG > maxIG:
+            maxIG = IG
+            maxIndex = index
+    return maxIndex
+    
+            
 trainfile = "DT_data/train.csv"
 x_train = []
 with open(trainfile,'r') as filename:
@@ -39,27 +94,10 @@ with open(trainfile,'r') as filename:
     for row in csvreader:
         x_train.append(row)
 
-labels = x_train[0]
-labels = labels[:-1]
-labels = [label.strip() for label in labels]
-x_train=x_train[1:]
-x_train = np.array(x_train)
+x_train= np.array(x_train)
+labels = x_train[0,:]
+x_train = x_train[1:,:]
 
-y_train = x_train[:,-1]
-y_train = np.array([(int)(y) for y in y_train])
-continuous= ["Age","Fnlwgt","Education Number","Capital Gain","Capital Loss","Hour per Week"]
-
-
-label_array = []
 for i in range(len(labels)):
-    label = labels[i]
-    if label in continuous:
-        label_array.append(np.array([]))
-    else:
-        print(label+" not in continuous")
-        label_array.append(np.unique(x_train[:,i]))
-
-label_array = np.array(label_array)    
-training_encoder = one_hot_encoder(label_array)
-x_train_encoded = training_encoder.encode(x_train)
-        
+    if labels[i] in continuous:
+        x_train[:,i] = [ (int)(x.strip()) for x in x_train[:,i]]
