886d861a76bfa8645fbea1f8bb0623c7665c1acb
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-10-14 19:41:02 +0530

    updates

--- A4/plot.py
+++ A4/plot.py
@@ -7,11 +7,23 @@ Created on Mon Oct 14 16:26:40 2019
 #Depth,Nodes,train_acc,val_acc,test_acc
 import matplotlib.pyplot as plt
 import pandas as pd
-data = (pd.read_csv('data_customTree_GainRatio_Entropy.csv')).to_numpy()
-plt.plot(data[:,0],data[:,2])
 
-plt.plot(data[:,0],data[:,3])
+files = ["data_customTree_GainRatio_Entropy","data_customTree_GainRatio_Gini","data_customTree_InfoGain_Gini","data_customTree_InfoGain_Entropy"]
 
-plt.plot(data[:,0],data[:,4])
-plt.legend(["Train","Val","Test"])
-plt.show()
+for file in files:
+    
+    data = (pd.read_csv(file+".csv")).to_numpy()
+    
+    fig = plt.figure()
+    plt.plot(data[:,0],data[:,2])
+    
+    plt.plot(data[:,0],data[:,3])
+    
+    plt.plot(data[:,0],data[:,4])
+    plt.legend(["Train","Val","Test"])
+    plt.xlabel("Node Count")
+    plt.ylabel("Accuracy")
+    parameters = file.split("_")
+    plt.title("Not Post Pruned,  {}, {}".format(parameters[2],parameters[3]))
+    plt.show()
+    fig.savefig(file+'.png')
