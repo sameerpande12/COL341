203411f6e41f91feda2367e37a594d617682acd8
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-10-26 18:12:25 +0530

    working code for svm. utils in comments

--- A4/SVM/svm.py
+++ A4/SVM/svm.py
@@ -14,7 +14,7 @@ class SVM:
         self.weights = np.zeros(dimensions[0]*dimensions[1])
         self.b = 0
         
-    def train(self,x,y,reg=1,T=100000,k=200,projection_step = False):
+    def train(self,x,y,reg=1,T=50000,k=200,projection_step = False):
         """
         reg -> regularization
         T -> number of iterations
@@ -29,9 +29,6 @@ class SVM:
         b = self.b
         
         y = np.array([-1 if element == self.label1 else 1 for element in y])
-         
-        
-        
         
         for t in range(1,T+1):
             if(t % 1000 == 0):
@@ -39,21 +36,42 @@ class SVM:
             A = np.random.choice(x.shape[0],k,replace=False)
             yi_xi = np.zeros(x.shape[1])
             yi = 0
+            
             for a in A:
                 if 1 - y[a]*(np.dot(w,x[a]) + b) > 0:
+                    
                     yi_xi = yi_xi + y[a] * x[a]
                     yi = yi + y[a]
-             
+            
+            
             w = w * ( 1 - 1/t) + 1/(reg*k*t) * yi_xi
             b = b + 1/(reg* k * t) * yi
             
         self.weights = w
         self.b = b
+    
+    def predict(self,prediction_set):
+        pred = [ np.dot(s,self.weights) + self.b for s in prediction_set]
+        pred = [ self.label1 if p < 0 else 1 for p in pred]
+        return pred
+"""
+y_labels = np.unique(train_data[:,-1])
 
+svm_classifiers = []
+numFeatures = train_data.shape[1]-1
+for i in range(0,len(y_labels)):
+    for j in range(i+1,len(y_labels)):
+        svm = SVM(y_labels[i],y_labels[j])
+        train_set = train_data[  np.logical_or(train_data[:,numFeatures] == 0,train_data[:,numFeatures]==1)]
+        svm.train(train_set[:,:numFeatures],train_set[:,numFeatures])
+        svm_classifiers.append(svm)
+"""        
+        
+        
 set1 = train_data[  np.logical_or(train_data[:,784] == 0,train_data[:,784]==1)]
 svm = SVM(0,1)
-svm.train(set1[:,:784],set1[:,784],10000)
-pred = [ np.dot(s[:784],svm.weights) for s in set1]
+svm.train(set1[:,:784],set1[:,784],1,1000,200)
+pred = [ np.dot(s[:784],svm.weights) + svm.b for s in set1]
 
 y = [ 0 if p < 0 else 1 for p in pred]
 count = 0
