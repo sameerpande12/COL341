80b8dcbd930c75aa183280639c97445e6f58629f
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-10-27 12:48:09 +0530

    working code for svm

--- A4/SVM/svm.py
+++ A4/SVM/svm.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-
+from matplotlib import pyplot as plt
 train_data = np.loadtxt( "SVM_data/train.csv",delimiter=',')
-
+test_data = np.loadtxt("SVM_data/test_public.csv",delimiter=',')
+test_labels = np.loadtxt("SVM_data/test_labels.txt")
 
 
 class SVM:
@@ -31,8 +32,7 @@ class SVM:
         y = np.array([-1 if element == self.label1 else 1 for element in y])
         
         for t in range(1,T+1):
-            if(t % 1000 == 0):
-                print("     {}".format(t))
+            
             A = np.random.choice(x.shape[0],k,replace=False)
             yi_xi = np.zeros(x.shape[1])
             yi = 0
@@ -52,7 +52,7 @@ class SVM:
     
     def predict(self,prediction_set):
         pred = [ np.dot(s,self.weights) + self.b for s in prediction_set]
-        pred = [ self.label1 if p < 0 else 1 for p in pred]
+        pred = [ self.label1 if p < 0 else self.label2 for p in pred]
         return pred
 
 y_labels = np.sort(np.unique(train_data[:,-1]))
@@ -62,32 +62,48 @@ numFeatures = train_data.shape[1]-1
 for i in range(0,len(y_labels)):
     for j in range(i+1,len(y_labels)):
         #print( "SVM CLASSIFIER #{}".format(i * len(y_labels)  + j + 1))
+        print("Training svm classifier number for labels : {} {}".format(y_labels[i],y_labels[j]))
         svm = SVM(y_labels[i],y_labels[j])
-        train_set = train_data[  np.logical_or(train_data[:,numFeatures] == 0,train_data[:,numFeatures]==1)]
-        svm.train(train_set[:,:numFeatures],train_set[:,numFeatures],1,1000,200)
+        train_set = train_data[  np.logical_or(train_data[:,numFeatures] == y_labels[i],train_data[:,numFeatures]==y_labels[j])]
+        svm.train(train_set[:,:numFeatures],train_set[:,numFeatures],1,2000,200)
         svm_classifiers.append(svm)
         
 
-def predict_sample(x,svm_classifiers,y_labels):
+def predict_sample(x,svm_classifiers=svm_classifiers,y_labels=y_labels):
     count = {}
     for label in y_labels:
         count[label] = 0
     for svm in svm_classifiers:
-        value = svm.predict(x)
+        value = svm.predict(np.array([x]))
+        #print(" {} vs {} -> {}".format(svm.label1,svm.label2,value))
         count [value[0]] = count[value[0]] + 1
-    
+    #print(count)
     return max(count,key = lambda k : count[k])
-    
-        
-        """
-set1 = train_data[  np.logical_or(train_data[:,784] == 0,train_data[:,784]==1)]
-svm = SVM(0,1)
-svm.train(set1[:,:784],set1[:,784],1,1000,200)
-pred = [ np.dot(s[:784],svm.weights) + svm.b for s in set1]
 
-y = [ 0 if p < 0 else 1 for p in pred]
 count = 0
-for i in range(len(y)):
-    if y[i] == set1[i,-1]:
-        count = count + 1
-"""
+for i in range(len(train_data)):
+    
+    x = train_data[i,:-1]
+    accurate_label = train_data[i,-1]
+    
+    predicted_label = (int)(predict_sample(x))
+    if accurate_label == predicted_label:
+        count = count+ 1
+    #print("{} : correct =  {}, predicted = {}".format(i+1,accurate_label,predicted_label))
+
+print("accuracy: {}".format(100 * count/len(train_data)))
+
+
+test_data[:,-1] = test_labels
+
+count = 0
+for i in range(len(test_data)):
+    
+    x = test_data[i,:-1]
+    accurate_label = test_data[i,-1]
+    
+    predicted_label = (int)(predict_sample(x))
+    if accurate_label == predicted_label:
+        count = count+ 1    
+
+print("accuracy: {}".format(100 * count/len(test_data)))
