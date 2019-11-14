4db87b7232546e4425b18f43f99177646893489c
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-11-14 19:43:52 +0530

    using portstemming, stopwords, bigrams,unigrams

--- A5/naive_bayes_c.py
+++ A5/naive_bayes_c.py
@@ -1,14 +1,206 @@
 import numpy as np
 import sys
 import pandas as pd
-#import time
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import time
+from nltk.stem import PorterStemmer
+
+
+
+
+
+stop_words = {'a',
+ 'about',
+ 'above',
+ 'after',
+ 'again',
+ 'against',
+ 'ain',
+ 'all',
+ 'am',
+ 'an',
+ 'and',
+ 'any',
+ 'are',
+ 'aren',
+ "aren't",
+ 'as',
+ 'at',
+ 'be',
+ 'because',
+ 'been',
+ 'before',
+ 'being',
+ 'below',
+ 'between',
+ 'both',
+ 'but',
+ 'by',
+ 'can',
+ 'couldn',
+ "couldn't",
+ 'd',
+ 'did',
+ 'didn',
+ "didn't",
+ 'do',
+ 'does',
+ 'doesn',
+ "doesn't",
+ 'doing',
+ 'don',
+ "don't",
+ 'down',
+ 'during',
+ 'each',
+ 'few',
+ 'for',
+ 'from',
+ 'further',
+ 'had',
+ 'hadn',
+ "hadn't",
+ 'has',
+ 'hasn',
+ "hasn't",
+ 'have',
+ 'haven',
+ "haven't",
+ 'having',
+ 'he',
+ 'her',
+ 'here',
+ 'hers',
+ 'herself',
+ 'him',
+ 'himself',
+ 'his',
+ 'how',
+ 'i',
+ 'if',
+ 'in',
+ 'into',
+ 'is',
+ 'isn',
+ "isn't",
+ 'it',
+ "it's",
+ 'its',
+ 'itself',
+ 'just',
+ 'll',
+ 'm',
+ 'ma',
+ 'me',
+ 'mightn',
+ "mightn't",
+ 'more',
+ 'most',
+ 'mustn',
+ "mustn't",
+ 'my',
+ 'myself',
+ 'needn',
+ "needn't",
+ 'no',
+ 'nor',
+ 'not',
+ 'now',
+ 'o',
+ 'of',
+ 'off',
+ 'on',
+ 'once',
+ 'only',
+ 'or',
+ 'other',
+ 'our',
+ 'ours',
+ 'ourselves',
+ 'out',
+ 'over',
+ 'own',
+ 're',
+ 's',
+ 'same',
+ 'shan',
+ "shan't",
+ 'she',
+ "she's",
+ 'should',
+ "should've",
+ 'shouldn',
+ "shouldn't",
+ 'so',
+ 'some',
+ 'such',
+ 't',
+ 'than',
+ 'that',
+ "that'll",
+ 'the',
+ 'their',
+ 'theirs',
+ 'them',
+ 'themselves',
+ 'then',
+ 'there',
+ 'these',
+ 'they',
+ 'this',
+ 'those',
+ 'through',
+ 'to',
+ 'too',
+ 'under',
+ 'until',
+ 'up',
+ 've',
+ 'very',
+ 'was',
+ 'wasn',
+ "wasn't",
+ 'we',
+ 'were',
+ 'weren',
+ "weren't",
+ 'what',
+ 'when',
+ 'where',
+ 'which',
+ 'while',
+ 'who',
+ 'whom',
+ 'why',
+ 'will',
+ 'with',
+ 'won',
+ "won't",
+ 'wouldn',
+ "wouldn't",
+ 'y',
+ 'you',
+ "you'd",
+ "you'll",
+ "you're",
+ "you've",
+ 'your',
+ 'yours',
+ 'yourself',
+ 'yourselves'}
+ 
+
+ps = PorterStemmer()
+
+
 def cleanse_String(line):
-    line = line.strip()
     line = line.lower()
     line =  line.replace('.',' ') 
     line =  line.replace(',',' ') 
-    line =  line.replace('\\','') ##make sure don't replace "don\'t" by "dont"
-    line =  line.replace('\'','') 
+    line =  line.replace('\\','') ##make sure don't replace "don\'t" by "don't"
+    line =  line.replace('\'','')
     line =  line.replace('"',' ') 
     line =  line.replace('-',' ') 
     line =  line.replace('!',' ') 
@@ -20,23 +212,34 @@ def cleanse_String(line):
     line =  line.replace('=',' ') 
     line =  line.replace('/',' ')
     line =  line.replace('\n',' ')
+    
+    word_tokens = line.split()
+    words_filtered = [ ps.stem(word) for word in word_tokens if not word in stop_words]
+    line = ''
+    for word in words_filtered:
+        line = line +word + ' '
+    
+    line.strip()
+    
     return line
 
 
-#trainfile = sys.argv[1]
-trainfile = 'traindata.csv'
+trainfile = sys.argv[1]
+testfilename = sys.argv[2]
+outputfile = sys.argv[3]
 
-#testfilename = sys.argv[2]
-testfilename = 'testdata.csv'
+#trainfile = 'traindata.csv'
+#testfilename = 'testdata.csv'
+#outputfile = 'output.txt'
 
-#outputfile = sys.argv[3]
-outputfile = 'output.txt'
+
 
 x_train = pd.read_csv(trainfile,sep=',',dtype=str).values
-x_train = x_train[1:]
+
 for i in range(len(x_train)):
     x_train[i][0] = cleanse_String(x_train[i][0])
 
+x_test = pd.read_csv(testfilename,sep=',').values
 
 positive = x_train[x_train[:,-1]=='positive']
 negative = x_train[x_train[:,-1]=='negative']
@@ -236,14 +439,14 @@ for (line,pred) in x_train:
     count = count + 1
     
     print(value,correct,count, correct/count)
-    
-"""
+"""    
+
 #============================================================================
         #Part of code for test purposes
 #============================================================================
         
 
-x_test = pd.read_csv(testfilename,sep=',').values
+
 
 f = open(outputfile,'w+')
 count = 0
@@ -254,4 +457,3 @@ for i in range(len(x_test)):
     count = count + 1
     #print(count)
 f.close()
-
